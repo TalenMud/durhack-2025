8ad450dcfc21b76d64fae7b7ba4d1335fcda6f90
Arryl <arryl@arryls-air.wlan.dur.ac.uk>
2025-11-02 00:49:30 +0000

    header

--- stargazing-supervisor/src/App.tsx
+++ stargazing-supervisor/src/App.tsx
@@ -1,11 +1,17 @@
 // import React from 'react';
 import Game from './components/Game';
+import Header from './components/Header';
 // import Chat from './Chat';
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+
+
+
 
 function App() {
   return (<>
     <div>
+      <Header />
       <h1>Who's that historical person?</h1>
       <Game />
       {/* <Chat /> */}
---
+++ stargazing-supervisor/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+function Header(){
+    return(
+         <h1 className="text-3xl font-bold">ChronoBridge</h1>
+    );
+}
+
+export default Header;
